0383bdc1e7132da788f2e654d531edfdd3a5eff3
chebyrek4 <moshenski@gmail.com>
2015-09-07 14:25:54 +0300

    First itteration

---
+++ brilliant/js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 400);
+
+    $('body').animate({
+      left: "285px"
+    }, 400);
+  });
+
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 400);
+
+    $('body').animate({
+      left: "0px"
+    }, 400);
+  });
+  
+
+});
